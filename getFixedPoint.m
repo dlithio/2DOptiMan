a8a242c1b75b8d824435249a0c05a300ff02035d
Dan L <dlithio.twitter@gmail.com>
2013-10-29 13:25:57 -0400

    balanced the delimiters

--- getFixedPoint.m
+++ getFixedPoint.m
@@ -2,10 +2,10 @@ function fixedPoint = getFixedPoint(myVectorField,guess,tolerance)
 err = Inf;
 
 while abs(err) > tolerance
-    myJac = jacobianest(myVectorField,guess;
+    myJac = jacobianest(myVectorField,guess);
     newGuess = guess' - myJac ^ -1 * myVectorField(guess)';
     err = guess - newGuess';
-    guess = newGuess;
+    guess = newGuess';
 end
 
-fixedPoint = guess;
+fixedPoint = guess';
