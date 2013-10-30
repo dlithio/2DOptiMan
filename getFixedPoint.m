b4cac45da341d9702684985d2d13000f1c53f345
Dan L <dlithio.twitter@gmail.com>
2013-10-30 11:02:05 -0400

    Added condition for exact

--- getFixedPoint.m
+++ getFixedPoint.m
@@ -1,11 +1,14 @@
 function fixedPoint = getFixedPoint(myVectorField,guess,tolerance)
 err = Inf;
 
-while abs(err) > tolerance
-    myJac = jacobianest(myVectorField,guess);
-    newGuess = guess' - myJac ^ -1 * myVectorField(guess)';
-    err = guess - newGuess';
-    guess = newGuess';
+currentOutput = myVectorField(guess);
+if norm(currentOutput) ~= 0
+    while abs(err) > tolerance
+        myJac = jacobianest(myVectorField,guess);
+        newGuess = guess' - myJac ^ -1 * myVectorField(guess)';
+        err = guess - newGuess';
+        guess = newGuess';
+    end
 end
 
 fixedPoint = guess';
