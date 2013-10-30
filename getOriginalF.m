a60406be973cd95e3caa1ad1e0b4a7acf4df626f
Dan L <dlithio.twitter@gmail.com>
2013-10-30 11:02:16 -0400

    supressed output

--- getOriginalF.m
+++ getOriginalF.m
@@ -5,7 +5,7 @@ originalF = zeros(mydim(1),mydim(2));
 
 for point=1:length(curve)
     vectorFieldAtPoint = myVectorField(curve(:,point));
-    vectorFieldAtPoint = vectorFieldAtPoint/norm(vectorFieldAtPoint)
+    vectorFieldAtPoint = vectorFieldAtPoint/norm(vectorFieldAtPoint);
     originalF(1,point) = vectorFieldAtPoint(1);
     originalF(2,point) = vectorFieldAtPoint(2);
     originalF(3,point) = vectorFieldAtPoint(3);
