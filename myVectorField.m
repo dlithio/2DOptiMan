530772b08d4091f2daf0f275f7f38e7de975771a
Dan L <dlithio.twitter@gmail.com>
2013-10-30 11:02:33 -0400

    commenting

--- myVectorField.m
+++ myVectorField.m
@@ -3,40 +3,40 @@ function [fOriginal fixedPointGuess radius timeTotal timeStepSize] = myVectorFie
 %%%%%%%%%%%%%%%%%%%
 %Lorenz System
 %%%%%%%%%%%%%%%%%%%
-%These first 3 lines are the actual Lorenz Equations
-fOriginal(1)=10*(point(2)-point(1));
-fOriginal(2)=28*point(1)-point(2)-point(1) .* point(3);
-fOriginal(3)=point(1).*point(2) - 8/3 * point(3);
-%In order to find the stable manifold, we reverse time. This program
-%automatically finds the unstable manifold.
-fOriginal(1)=-fOriginal(1);
-fOriginal(2)=-fOriginal(2);
-fOriginal(3)=-fOriginal(3);
-%The following constants need to be specified for any vectorfield
-fixedPointGuess = [0 0 0]; %Can be the exact fixed point, but doesn't need to be
-radius = 3; %How close initial points are to the fixed point
-timeTotal = 20; %How long the system is evolved for
-timeStepSize = .5; %How far we go in each timeStep
+% %These first 3 lines are the actual Lorenz Equations
+% fOriginal(1)=10*(point(2)-point(1));
+% fOriginal(2)=28*point(1)-point(2)-point(1) .* point(3);
+% fOriginal(3)=point(1).*point(2) - 8/3 * point(3);
+% %In order to find the stable manifold, we reverse time. This program
+% %automatically finds the unstable manifold.
+% fOriginal(1)=-fOriginal(1);
+% fOriginal(2)=-fOriginal(2);
+% fOriginal(3)=-fOriginal(3);
+% %The following constants need to be specified for any vectorfield
+% fixedPointGuess = [0 0 0]; %Can be the exact fixed point, but doesn't need to be
+% radius = 3; %How close initial points are to the fixed point
+% timeTotal = 90; %How long the system is evolved for
+% timeStepSize = .5; %How far we go in each timeStep
 
 %%%%%%%%%%%%%%%%%%%
 % A Dali Manifold
 %%%%%%%%%%%%%%%%%%%
-% fOriginal(1) = point(1);
-% fOriginal(2) = point(2);
-% if (point(1) < -.5)
-%     Phi=.1*(point(1)+.5)^3;
-%     Phix=.3*(point(1)+.5)^2;
-%     Phiy=0;
-%     fOriginal(3) =Phix*fOriginal(1)+Phiy*fOriginal(2)-(point(3)-Phi);
-% else
-%     fOriginal(3)=0;
-% end
-%%%%%%%%%%%%%%%%%
-% fixedPointGuess = [ 0 0 0]; % F(fixedPoint) =0 
-% radius = .4; %How close initial points are to fixed point
-% timeTotal = 4; %How long the system is evolved for
-% timeStepSize = .1;
-%%%%%%%%%%%%%%%%%
+fOriginal(1) = point(1);
+fOriginal(2) = point(2);
+if (point(1) < -.5)
+    Phi=.1*(point(1)+.5)^3;
+    Phix=.3*(point(1)+.5)^2;
+    Phiy=0;
+    fOriginal(3) =Phix*fOriginal(1)+Phiy*fOriginal(2)-(point(3)-Phi);
+else
+    fOriginal(3)=0;
+end
+%%%%%%%%%%%%%%%%
+fixedPointGuess = [0 0 0]; % F(fixedPoint) =0 
+radius = .4; %How close initial points are to fixed point
+timeTotal = 4; %How long the system is evolved for
+timeStepSize = .1;
+%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%
 % A Sundial Manifold
@@ -58,11 +58,11 @@ timeStepSize = .5; %How far we go in each timeStep
 %     Phith=0;
 % end
 % fOriginal(3)=Phir*radius+Phith*.1-(z-Phi);
-%%%%%%%%%%%%%%%%%
-% fixedPoint = [ 0 0 0]; % F(fixedPoint) =0 
+% %%%%%%%%%%%%%%%%
+% fixedPointGuess = [ 0 0 0]; % F(fixedPoint) =0 
 % radius = .3; %How close initial points are to fixed point
 % timeTotal = .3; %How long the system is evolved for
-% timeStepSize = .1;
-%%%%%%%%%%%%%%%%%
+% timeStepSize = .001;
+% %%%%%%%%%%%%%%%%
 
 end
