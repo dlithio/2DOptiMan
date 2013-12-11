84636214218c1f50f0e2ea63388e5a08de6fa951
dlithio <dlithio@users.noreply.github.com>
2013-12-10 21:17:33 -0500

    Delete getNextLocalThisRing.m

--- getNextLocalThisRing.m
+++
@@ -1,7 +0,0 @@
-function nextLocalThisRing = getNextLocalThisRing(local,timestep,pointsUsed2)
-    if local == pointsUsed2(timestep)
-        nextLocalThisRing = 1;
-    else
-        nextLocalThisRing = local + 1;
-    end
-end
