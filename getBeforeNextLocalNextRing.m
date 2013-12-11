a31c065f62f8e34c0874b9e8c35afd22fc31ecf5
dlithio <dlithio@users.noreply.github.com>
2013-12-10 21:16:47 -0500

    Delete getBeforeNextLocalNextRing.m

--- getBeforeNextLocalNextRing.m
+++
@@ -1,5 +0,0 @@
-function beforeNextLocalNextRing = getBeforeNextLocalNextRing(local,timestep,pointsUsed2,index)
-    beforeNextLocalNextRing = getNextLocalNextRing(local,timestep,pointsUsed2,index) - 1;
-    if beforeNextLocalNextRing == 0
-        beforeNextLocalNextRing = pointsUsed2(timestep+1);
-end
