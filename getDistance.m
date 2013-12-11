286ce7688b3c4d33973ab99d4da5855416f28fc0
dlithio <dlithio@users.noreply.github.com>
2013-12-10 21:16:51 -0500

    Delete getDistance.m

--- getDistance.m
+++
@@ -1,9 +0,0 @@
-function distanceNonNormal = getDistance(curve)
-
-curveBig = [curve curve curve];
-curvePlus1 = [curve(:,2:end) curve curve curve(:,1)];
-
-difference = curveBig-curvePlus1;
-difference = difference(:,length(curve)+1:2*length(curve));
-
-distanceNonNormal = (sum(difference .^ 2)) .^ (.5);
