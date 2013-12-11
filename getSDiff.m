371fdcb9cac6845e47a4ceb32e82ff1a7d09b4c5
dlithio <dlithio@users.noreply.github.com>
2013-12-10 21:17:28 -0500

    Delete getSDiff.m

--- getSDiff.m
+++
@@ -1,8 +0,0 @@
-function sDiff = getSDiff(s)
-
-sBig = [s s+2*pi s+4*pi];
-sBigMinus1 = [sBig(:,end) sBig(:,1:end-1)];
-sDiffBig = sBig - sBigMinus1;
-sDiff = sDiffBig(length(s)+1:2*length(s));
-
-end
