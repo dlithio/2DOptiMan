328826d891e0badea09585f35c2ae59f0e58fda0
dlithio <dlithio@users.noreply.github.com>
2013-12-10 21:18:18 -0500

    Delete getNextLocalNextRing.m

--- getNextLocalNextRing.m
+++
@@ -1,3 +0,0 @@
-function nextLocalNextRing = getNextLocalNextRing(local,timestep,pointsUsed2,index)
-    nextLocalNextRing = find(index(:,timestep+1) == index(getNextLocalThisRing(local,timestep,pointsUsed2),timestep));
-end
