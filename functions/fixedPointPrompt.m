3a0aadee72841c45b2aaca10f5fcd1670d824dfb
dlithio <dlithio.twitter@gmail.com>
2013-12-10 22:09:39 -0500

    deleted new line that was annoying

--- functions/fixedPointPrompt.m
+++ functions/fixedPointPrompt.m
@@ -2,7 +2,7 @@ function fixedPoint = fixedPointPrompt(fixedPointGuess)
 
 if isvector(fixedPointGuess) && isequal(size(fixedPointGuess),[1 3])
     fixedPoint = getFixedPoint(@myVectorField,fixedPointGuess,1e-10);
-    disp(sprintf('The guessed fixed point is [%d %d %d]. If this is correct, hit enter. \n Otherwise, enter a new guess. \n',fixedPoint))
+    disp(sprintf('The guessed fixed point is [%d %d %d]. If this is correct, hit enter. \n Otherwise, enter a new guess.',fixedPoint))
     newfixedPointGuess = input('Use the format [0 0 0], including the brackets. \n');
     if isempty(newfixedPointGuess)
         fixedPoint = fixedPoint;
