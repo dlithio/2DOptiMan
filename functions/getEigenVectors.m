b7e5f86a7391f7da98f5d7109b255b2fa7260060
Dan L <dlithio.twitter@gmail.com>
2014-02-05 02:58:46 -0500

    Get eigenvalues too

--- functions/getEigenVectors.m
+++ functions/getEigenVectors.m
@@ -1,18 +1,26 @@
-function [eig1 eig2] = getEigenVectors(myVectorField,fixedPoint)
+function [eig_vec1 eig_vec2 eig_val1 eig_val2] = getEigenVectors(myVectorField,fixedPoint)
 
 myJac = jacobianest(myVectorField,fixedPoint);
 [v,d]=eig(myJac);
 myNums = [];
+myNumsNeg = [];
 
 for i = 1:3
     if d(i,i) > 0;
         myNums = [myNums i];
+    else
+        myNumsNeg = [myNumsNeg i];
     end
 end
 
 if length(myNums) == 2
-    eig1 = v(:,myNums(1));
-    eig2 = v(:,myNums(2));
+    eig_vec1 = v(:,myNums(1));
+    eig_val1 = myNums(1);
+    eig_vec2 = v(:,myNums(2));
+    eig_val2 = myNums(2);
 else
-    error('There are not 2 positive eigenvectors. Try reversing time in your vector field (make everything negative)');
+    eig_vec1 = v(:,myNumsNeg(1));
+    eig_val1 = -myNumsNeg(1);
+    eig_vec2 = v(:,myNumsNeg(2));
+    eig_val2 = -myNumsNeg(2);
 end
